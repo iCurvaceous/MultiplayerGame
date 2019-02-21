91705b815f6fe302f064ddcf22cbdcaf80d16529
deCastro <adecastro@labs.neit.edu>
2019-02-21 15:14:22 -0500

    day 2 week 7 class

--- app.js
+++ app.js
@@ -1,25 +1,47 @@
 var io = require('socket.io')(process.env.PORT || 5000);
+var shortid = require("shortid");
+var players = [];
 var playerCount = 0;
 console.log("Server Running");
 
 io.on('connection', function(socket){
     console.log("Connected to Unity");
+    var thisPlayerId = shortid.generate();
 
-    socket.broadcast.emit('spawn');
-    playerCount++;
+    var player = {
+        id:thisPlayerId,
+        position:{
+            v:0
+        }
+    }
+
+    players[thisPlayerId] = player;
+    socket.broadcast.emit('spawn', {id:thisPlayerId});
+    console.log("SERVER LOG: Sending spawn to new with ID", thisPlayerId);
+    console.log("players array length: ", players.length);
 
     for(var i = 0; i < playerCount; i++){
         socket.emit('spawn');
-        console.log('sending spawn to new player');
+
+        PlayerCount++;
     }
 
     socket.on('sayhello', function(data){
-        console.log("Unity Game says hello");
+        console.log("SERVER LOG: Unity Game says hello");
         socket.emit('talkback');
     });
 
 
-socket.on('disconnect', function(){
-    console.log("Player Disconnected")
-})
+    socket.on('disconnect', function(){
+        console.log("SERVER LOG: Player Disconnected");
+        //playerCount--;
+    })
+
+    socket.on('move', function(data){
+        data.id = thisPlayerId;
+        //console.log('Player has Moved', JSON.stringify(data));
+        socket.broadcast.emit('move', data);
+    });
+
+    console.log("SERVER LOG--> Number of players connected: " + players.length)
 });
